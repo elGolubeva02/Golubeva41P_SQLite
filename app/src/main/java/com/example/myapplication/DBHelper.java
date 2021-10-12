b8806274b6e153e7b997c3856b49e878482a477f
Елизавета Голубева <elizabeththefirst2002@gmail.com>
2021-10-12 15:44:21 +0300

    Library

--- app/src/main/java/com/example/myapplication/DBHelper.java
+++ app/src/main/java/com/example/myapplication/DBHelper.java
@@ -3,23 +3,24 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper  extends SQLiteOpenHelper{
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
+    public static final String TABLE_BOOKS = "books";
     public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_PHOUSE = "phouse";
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
-                + " integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
+        db.execSQL("create table " + TABLE_BOOKS + "(" + KEY_ID
+                + " integer primary key," + KEY_NAME + " text," + KEY_AUTHOR + " text," + KEY_PHOUSE + " text" + ")");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_CONTACTS);
+        db.execSQL("drop table if exists " + TABLE_BOOKS);
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,80 +5,152 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CursorAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.w3c.dom.Text;
 //Главный класс
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    Button button1, button2, button3;
-    EditText Txt1, Txt2;
-    TextView textView;
-    String vivod;
+    Button button1, button3;
+    EditText Txt1, Txt2, Txt3;
     DBHelper dbHelper;
+    SQLiteDatabase database;
+    ContentValues contentValues;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(this);
         button3 = (Button) findViewById(R.id.button3);
         button3.setOnClickListener(this);
         Txt1 = (EditText) findViewById(R.id.Txt1);
         Txt2 = (EditText) findViewById(R.id.Txt2);
-        textView = (TextView) findViewById(R.id.textView);
+        Txt3 = (EditText) findViewById(R.id.Txt3);
         dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+        UpdateTable();
     }
+
+    public void UpdateTable()
+    {
+        Cursor cursor = database.query(DBHelper.TABLE_BOOKS, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
+            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
+            int authorIndex = cursor.getColumnIndex(DBHelper.KEY_AUTHOR);
+            int phouseIndex = cursor.getColumnIndex(DBHelper.KEY_PHOUSE);
+            TableLayout tblay = findViewById(R.id.tblay);
+            tblay.removeAllViews();
+            do {
+                TableRow tblayrow = new TableRow(this);
+                tblayrow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                TableRow.LayoutParams params = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                TextView outputid = new TextView(this);
+                params.weight = 1.0f;
+                outputid.setLayoutParams(params);
+                outputid.setText(cursor.getString(idIndex));
+                tblayrow.addView(outputid);
+                TextView outputname = new TextView(this);
+                params.weight = 3.0f;
+                outputname.setLayoutParams(params);
+                outputname.setText(cursor.getString(nameIndex));
+                tblayrow.addView(outputname);
+                TextView outputauthor = new TextView(this);
+                params.weight = 3.0f;
+                outputauthor.setLayoutParams(params);
+                outputauthor.setText(cursor.getString(authorIndex));
+                tblayrow.addView(outputauthor);
+                TextView outputphouse = new TextView(this);
+                params.weight = 3.0f;
+                outputphouse.setLayoutParams(params);
+                outputphouse.setText(cursor.getString(phouseIndex));
+                tblayrow.addView(outputphouse);
+                Button deletbt = new Button(this);
+                deletbt.setOnClickListener(this);
+                params.weight = 3.0f;
+                deletbt.setLayoutParams(params);
+                deletbt.setText("Удалить запись");
+                deletbt.setId(cursor.getInt(idIndex));
+                tblayrow.addView(deletbt);
+                tblay.addView(tblayrow);
+
+
+            } while (cursor.moveToNext());
+
+        } else
+
+
+
+
+        cursor.close();
+    }
+
     @Override
     public void onClick(View v) {
-        String name = Txt1.getText().toString();
-        String email = Txt2.getText().toString();
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
         switch (v.getId()) {
             case R.id.button1:
+                String name = Txt1.getText().toString();
+                String author = Txt2.getText().toString();
+                String phouse = Txt3.getText().toString();
+                contentValues = new ContentValues();
                 contentValues.put(DBHelper.KEY_NAME, name);
-                contentValues.put(DBHelper.KEY_MAIL, email);
-
-                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
-                break;
-            case R.id.button2:
-                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                } else
-                    Log.d("mLog","0 rows");
-
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
-                    do {
-                        vivod += ("ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                    textView.setText(vivod.toString());
-                } else
-                    Log.d("mLog","0 rows");
-
-                cursor.close();
+                contentValues.put(DBHelper.KEY_AUTHOR, author);
+                contentValues.put(DBHelper.KEY_PHOUSE, phouse);
+                database.insert(DBHelper.TABLE_BOOKS, null, contentValues);
+                UpdateTable();
+                Txt1.setText(null);
+                Txt2.setText(null);
+                Txt3.setText(null);
                 break;
             case R.id.button3:
-                database.delete(DBHelper.TABLE_CONTACTS, null, null);
+                database.delete(DBHelper.TABLE_BOOKS, null, null);
+                TableLayout dbOutput = findViewById(R.id.tblay);
+                dbOutput.removeAllViews();
+                Txt1.setText(null);
+                Txt2.setText(null);
+                Txt3.setText(null);;
+                UpdateTable();
+                break;
+            default:
+                View outputDBRoe = (View) v.getParent();
+                ViewGroup outputDB = (ViewGroup) outputDBRoe.getParent();
+                outputDB.removeView(outputDBRoe);
+                outputDB.invalidate();
+                database.delete(DBHelper.TABLE_BOOKS,DBHelper.KEY_ID + " = ?", new String[] {String.valueOf(v.getId())});
+                contentValues = new ContentValues();
+                Cursor cursorUpdater = database.query(DBHelper.TABLE_BOOKS, null, null, null, null, null, null);
+                if (cursorUpdater.moveToFirst()) {
+                    int idIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_ID);
+                    int nameIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_NAME);
+                    int authorIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_AUTHOR);
+                    int phouseIndex = cursorUpdater.getColumnIndex(DBHelper.KEY_PHOUSE);
+                    int realId = 1;
+                    do {
+                        if (cursorUpdater.getInt(idIndex) > realId) {
+                            contentValues.put(DBHelper.KEY_ID, realId);
+                            contentValues.put(DBHelper.KEY_NAME, cursorUpdater.getString(nameIndex));
+                            contentValues.put(DBHelper.KEY_AUTHOR, cursorUpdater.getString(authorIndex));
+                            contentValues.put(DBHelper.KEY_PHOUSE, cursorUpdater.getString(phouseIndex));
+                            database.replace(DBHelper.TABLE_BOOKS, null, contentValues);
+                        }
+                        realId++;
+                    } while (cursorUpdater.moveToNext());
+                    if (cursorUpdater.moveToLast() && v.getId()!=realId)
+                    {
+                        database.delete(DBHelper.TABLE_BOOKS, DBHelper.KEY_ID+ " = ?", new String[] {cursorUpdater.getString(idIndex)});
+                    }
+                  UpdateTable();
+                }
                 break;
         }
-        dbHelper.close();
     }
 }
